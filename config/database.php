8d4bfd7d4e3c92b799ca825bb1b3a036e5c7a975
Magali Fabri <magali_90@live.nl>
2022-04-20 07:05:36 +0200

    Try with db url, with default other params

--- config/database.php
+++ config/database.php
@@ -47,7 +47,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
+            'url' => getenv('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
@@ -68,11 +68,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => $dbParams["host"],
-            'port' => $dbParams["port"],
-            'database' => ltrim($dbParams["path"], "/"),
-            'username' => $dbParams["user"],
-            'password' => $dbParams["pass"],
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
